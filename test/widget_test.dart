2bc1e7fa2c38e3bbe95276d9cc6bce9c45ac9b55
Sarvarbek <fskelajak@gmail.com>
2021-08-30 07:07:37 +0500

    widget_test_change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,13 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:unit_widget_test/main.dart';
+import 'package:unit_widget_test/pages/detail_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-    expect(find.byType(Container), findsWidgets);
-    expect(find.byIcon(Icons.add), findsOneWidget);
-    expect(find.text("SetState"), findsWidgets);
+  testWidgets('Detail Page', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Detail Page"),
+        ),
+      ),
+    ));
+    expect(find.text('Detail Page'), findsOneWidget);
+    expect(find.byType(Text), findsWidgets);
   });
 }
